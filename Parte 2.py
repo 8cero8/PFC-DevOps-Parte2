bb7a4282c5691a4068e962e51470b0ff7a3136c9
Maxi GB <111007825+8cero8@users.noreply.github.com>
2022-09-02 12:31:21 -0300

    Se carga el código de la parte 2
    Se carga el código
    Parte 2
    Prueba DevOps

---
+++ Parte 2.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+@author: MrRobot
+Prueba DevOps 02-09-2022
+"""
+
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+
+# se carga el driver de google chrome
+driver = webdriver.Chrome("/Users/MrRobot/PythonProject/chromedriver")
+#CI 50379263  port 8111
+driver.get("http://localhost:8111")
+
+#Hacemos web scrapping y buscamos el campo para ingresar el usuario por ID
+elemento1 = driver.find_element(By.ID, "user")
+elemento1.clear()
+elemento1.send_keys("root")
+
+#Hacemos web scrapping y buscamos el campo para ingresar la contraseña por NAME
+elemento2 = driver.find_element(By.NAME, "pass")
+elemento2.clear()
+elemento2.send_keys("password")
+
+elemento2.submit()
+driver.get("http://localhost:8111")
+driver.execute_script('document.querySelector("#CreateTicketInQueue > div.create-wide > input").click()')
+elemento3 = driver.find_element(By.XPATH, '//*[@id="TitleBox--_Ticket_Create_html--messagedetails----Q3JlYXRlIGEgbmV3IHRpY2tldCBpbiBHZW5lcmFs---0"]/div/div/div[4]/div[2]/input')
+elemento3.click()
+elemento3.send_keys('testing')
+elemento3.submit()
+time.sleep(20)
+driver.close()
